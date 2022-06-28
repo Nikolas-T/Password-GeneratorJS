d1cba2c3f62d4b53e98ae0c096f4eb87b11c2657
6283726 627262663 <nikolastonge@hotmail.com>
2022-06-27 23:22:44 -0400

    added more code

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +15,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
+
+// Generator function
+function getRandomLower() {
+  return String.fromCharCode (Math.floor(Math.random() * 26) + 97 );
+
+}
+
+function getRandomUpper() {
+  return String.fromCharCode (Math.floor(Math.random() * 26) + 65 );
+
+}
+
+function getRandomNumber() {
+  return String.fromCharCode (Math.floor(Math.random() * 10) + 48 );
+
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()?+-~';
+  return symbols[Math.floor(Math.random() * symbols.length)]
+
+}
+
+console.log(getRandomLower())
