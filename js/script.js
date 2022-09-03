f0b25d100c0f63bf39db61913a80f5a0cf7ff7a6
Nikolas-T <105891869+Nikolas-T@users.noreply.github.com>
2022-09-03 14:06:42 -0400

    finally

--- js/script.js
+++ js/script.js
@@ -1,60 +1,66 @@
 // Assignment Code
 
+var choice = []
+var Length = 8;
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialChar = ["!", "@", "#", "$", "%", "&", "*", "_", "-", "+",]
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-const Length = 8;
-const choice = []
-const specialChar = ["!","@","#","$","%","&","*","_","-","+",]
-const lowerCase = ['a','B','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-const numbers = ['1','2','3','4','5','6','7','8','9','0'];
-const generateBtn = document.querySelector("#generate")
-
-generateBtn.addEventListener("click", writePassword);
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+var generateBtn = document.querySelector("#generate")
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
+
+function popup() {
+  choice = [];
+  Length = parseInt(prompt("How many characters do you want your password to be? (must be at least 8 characters)"));
+
+  if (isNaN(Length) || Length < 8 || Length > 128) {
+    alert("Character length has to be a number, 8 - 128 digits. Try again.")
+    return false;
+  }
+  if (confirm("Do you want uppercase letters?")) {
+    choice = choice.concat(upperCase);
+  }
+  if (confirm("Do you want lowercase letters in your password?")) {
+    choice = choice.concat(lowerCase);
+  }
+  if (confirm("Do you want special characters?")) {
+    choice = choice.concat(specialChar);
+  }
+
+  if (confirm("Do you want numbers?")) {
+    choice = choice.concat(numbers);
+  }
+  return true;
 }
+
+// Write password to the #password input
+function writePassword() {
+  var usablePassword = popup()
+  var passwordText = document.querySelector("#password");
+
+  if (usablePassword) {
+    var genPassword = generatePassword();
+    passwordText.value = genPassword;
+  }
+}
+
+
 
 // Generator function
-function getRandomLower() {
-  return String.fromCharCode (Math.floor(Math.random() * 26) + 97 );
-
+function generatePassword() {
+  var password = " ";
+  for (var i = 0; i < Length; i++) {
+    var randomize = Math.floor(Math.random() * choice.length);
+    password = password + choice[randomize];
+  }
+  return password;
 }
 
-function getRandomUpper() {
-  return String.fromCharCode (Math.floor(Math.random() * 26) + 65 );
 
-}
-
-function getRandomNumber() {
-  return String.fromCharCode (Math.floor(Math.random() * 10) + 48 );
-
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*()?+-~';
-  return symbols[Math.floor(Math.random() * symbols.length)]
-
-}
-
-console.log(getRandomLower())
 
 //this 
-
